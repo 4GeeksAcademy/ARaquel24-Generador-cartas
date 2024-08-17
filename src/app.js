9cb114cff19885cf4a649e7337741f71f5b9c701
ARaquel24 <anarklprzrjs@gmail.com>
2024-08-17 00:35:17 +0000

    finish

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const paloCarta = ["♦", "♥", "♠", "♣"];
+  const valorCarta = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const paloAleatorio = paloCarta[Math.floor(Math.random() * paloCarta.length)];
+  const valorAleatorio =
+    valorCarta[Math.floor(Math.random() * valorCarta.length)];
+  if (paloAleatorio == "♥") {
+    document.querySelector(".paloCard1").innerHTML = paloAleatorio;
+    document.querySelector(".paloCard2").innerHTML = paloAleatorio;
+    document.querySelector(".paloCard1").style.color = "red";
+    document.querySelector(".paloCard2").style.color = "red";
+  } else {
+    document.querySelector(".paloCard1").innerHTML = paloAleatorio;
+    document.querySelector(".paloCard2").innerHTML = paloAleatorio;
+  }
+  document.querySelector(".valorCard").innerHTML = valorAleatorio;
 };
